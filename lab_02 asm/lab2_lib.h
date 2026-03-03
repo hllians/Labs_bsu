bcbdb4c769064c3f08c79f893b28cd505273c31d
Ulyana Hlushanko <68190677+hllians@users.noreply.github.com>
2026-03-03 20:36:48 +0300

    Delete lab_02 asm directory

--- lab_02 asm/lab2_lib.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cstdint>
-
-int calcExpration1(int a);
-int calcExpration2(int a);
-void add48(int16_t A1, int16_t A2, int16_t A3,
-           int16_t B1, int16_t B2, int16_t B3,
-           int16_t& C1, int16_t& C2, int16_t& C3);
